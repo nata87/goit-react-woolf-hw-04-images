baec0f908b2c2345a91df358bc6beb6857b8f563
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-05-06 18:55:05 +0200

    refactoring

--- src/API.js
+++ src/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '42274052-b5209bd40b827282c0377ed93';
+
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export function getPhotoGallery(query, page, perPage) {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 import SearchBar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
@@ -18,73 +18,91 @@ const INITIAL_STATE = {
 
 const PER_PAGE = 12;
 
-export default class App extends Component {
-  state = { ...INITIAL_STATE };
+const App = () => {
+  const [state, setState] = useState({ ...INITIAL_STATE });
+  const {
+    page,
+    query,
+    galleryPhotos,
+    isLoading,
+    totalHits,
+    isModalOpen,
+    currentPhotoUrl,
+  } = state;
 
-  async componentDidUpdate(_, prevState) {
-    if (
-      this.state.page !== prevState.page ||
-      this.state.query !== prevState.query
-    ) {
-      this.setState({ isLoading: true });
+  useEffect(() => {
+    const setImages = async () => {
       try {
         const {
           data: { hits, totalHits },
-        } = await getPhotoGallery(this.state.query, this.state.page, PER_PAGE);
+        } = await getPhotoGallery(query, page, PER_PAGE);
         const newPhotos = hits.map(({ id, webformatURL, largeImageURL }) => {
           return { id, webformatURL, largeImageURL };
         });
-        this.setState({
-          galleryPhotos: [...this.state.galleryPhotos, ...newPhotos],
+        setState(prevState => ({
+          ...prevState,
+          galleryPhotos: [...prevState.galleryPhotos, ...newPhotos],
           totalHits,
           isLoading: false,
-        });
+        }));
       } catch (error) {
-        this.setState(...INITIAL_STATE);
+        setState({ ...INITIAL_STATE });
       }
-    }
-  }
+    };
 
-  handlerSubmit = query => {
-    this.setState({ ...INITIAL_STATE, query, page: 1 });
+    setImages();
+  }, [page, query]);
+
+  const handlerSubmit = query => {
+    setState(prevState => ({
+      ...INITIAL_STATE,
+      isLoading: true,
+      query,
+      page: 1,
+    }));
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+  const handleLoadMore = () => {
+    setState(prevState => ({
+      ...prevState,
+      page: prevState.page + 1,
+    }));
   };
 
-  handleModalClose = () => {
-    this.setState({ isModalOpen: false });
+  const handleModalClose = () => {
+    setState(prevState => ({
+      ...prevState,
+      isModalOpen: false,
+    }));
   };
 
-  handleModalOpen = largeImageURL => {
-    this.setState({ isModalOpen: true, currentPhotoUrl: largeImageURL });
+  const handleModalOpen = largeImageURL => {
+    setState(prevState => ({
+      ...prevState,
+      isModalOpen: true,
+      currentPhotoUrl: largeImageURL,
+    }));
   };
 
-  render() {
-    return (
-      <div className="App">
-        <SearchBar onSubmit={this.handlerSubmit} />
-        {this.state.galleryPhotos.length > 0 ? (
-          <ImageGallery
-            onModalClose={this.handleModalClose}
-            onModalOpen={this.handleModalOpen}
-            galleryPhotos={this.state.galleryPhotos}
-          />
-        ) : null}
-        {this.state.isLoading && <Loader />}
-        {this.state.page < Math.ceil(this.state.totalHits / PER_PAGE) ? (
-          <Button onClick={this.handleLoadMore}>Load more</Button>
-        ) : null}
-        {this.state.isModalOpen && (
-          <Modal
-            url={this.state.currentPhotoUrl}
-            onModalClose={this.handleModalClose}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <SearchBar onSubmit={handlerSubmit} />
+      {galleryPhotos.length > 0 ? (
+        <ImageGallery
+          onModalClose={handleModalClose}
+          onModalOpen={handleModalOpen}
+          galleryPhotos={galleryPhotos}
+        />
+      ) : null}
+      {isLoading && <Loader />}
+      {page < Math.ceil(totalHits / PER_PAGE) ? (
+        <Button onClick={handleLoadMore}>Load more</Button>
+      ) : null}
+      {isModalOpen && (
+        <Modal url={currentPhotoUrl} onModalClose={handleModalClose} />
+      )}
+    </div>
+  );
+};
+
+export default App;
